9443bee4d2306933ad787570a06b34e98a79ffa8
halilovick <khalilovic02@gmail.com>
2023-06-01 00:23:59 +0200

    O nama, Podrska views push

--- VoziMe/Controllers/HomeController.cs
+++ VoziMe/Controllers/HomeController.cs
@@ -23,6 +23,15 @@ namespace VoziMe.Controllers {
             return View();
         }
 
+        public IActionResult Onama()
+        {
+            return View();
+        }
+        public IActionResult Podrska()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
