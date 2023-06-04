60b6d3d2abcec3a8a7739b16e66d4e631f63d1b8
emiragovic <eagovic1@etf.unsa.ba>
2023-06-04 16:56:29 +0200

    db migrations small change

--- VoziMe/Migrations/ApplicationDbContextModelSnapshot.cs
+++ VoziMe/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -3,17 +3,15 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VoziMe.Data;
 
-namespace VoziMe.Data.Migrations
+namespace VoziMe.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20230531194601_initialMigration")]
-    partial class initialMigration
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
