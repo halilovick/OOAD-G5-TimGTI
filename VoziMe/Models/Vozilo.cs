1be8423f4fe39240e2dae47355122eff58a60918
halilovick <khalilovic02@gmail.com>
2023-06-08 12:21:15 +0200

    Minor change in Vozilo model

--- VoziMe/Models/Vozilo.cs
+++ VoziMe/Models/Vozilo.cs
@@ -1,14 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
 namespace VoziMe.Models {
-    public class Vozilo {
+    public class Vozilo
+    {
         public Vozilo() { }
         [Key]
         public int id { get; set; }
         public Proizvodjac proizvodjac { get; set; }
         public string model { get; set; }
         public int godinaProizvodnje { get; set; }
-        public int registarskaOznaka { get; set; }
+        public string registarskaOznaka { get; set; }
         public Boja boja { get; set; }
         public int brojSjedista { get; set; }
     }
