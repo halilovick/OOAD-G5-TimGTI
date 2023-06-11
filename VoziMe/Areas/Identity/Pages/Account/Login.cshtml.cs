b59b89222c913f5461ac2eb8429181e654f18be4
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2023-06-11 12:42:01 +0200

    Updated login

--- VoziMe/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ VoziMe/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -50,7 +50,7 @@ namespace VoziMe.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Zapamti me?")]
             public bool RememberMe { get; set; }
         }
 
@@ -98,7 +98,7 @@ namespace VoziMe.Areas.Identity.Pages.Account
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    ModelState.AddModelError(string.Empty, "Neuspješna prijava, pokušajte ponovo");
                     return Page();
                 }
             }
