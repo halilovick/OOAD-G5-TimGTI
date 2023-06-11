c55b5a05eae4085036aee1000639fb67165aede8
halilovick <khalilovic02@gmail.com>
2023-06-11 21:45:04 +0200

    user login commit

--- VoziMe/Controllers/HomeController.cs
+++ VoziMe/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Extensions.Logging;
 using System;
@@ -6,14 +8,25 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using VoziMe.Data;
 using VoziMe.Models;
 
 namespace VoziMe.Controllers {
     public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
 
-        public HomeController(ILogger<HomeController> logger) {
+        /*public HomeController(ILogger<HomeController> logger) {
             _logger = logger;
+        }*/
+
+        public HomeController(ApplicationDbContext context, UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public IActionResult Index() {
@@ -36,8 +49,32 @@ namespace VoziMe.Controllers {
         {
             return View();
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            var useri = _context.Klijent.ToList();
+            foreach (Klijent user in useri)
+            {
+                if (user.mailAdresa == email && user.lozinka == password)
+                {
+                    KlijentController.klijentLokalno = user;
+                    var userASP = await _userManager.GetUserAsync(User);
+                    Console.WriteLine(userASP.Email);
+                    await _userManager.AddToRoleAsync(userASP, "Korisnik");
+                    //return Task.FromResult<IActionResult>(RedirectToAction("Index"));
+                    await _signInManager.RefreshSignInAsync(userASP);
+                    return RedirectToAction("Index");
+                }
+            }
+            return RedirectToAction("Onama");
+        }
+
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
